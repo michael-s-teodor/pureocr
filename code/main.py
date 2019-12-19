3a3d1bcead5519632ab237d3cff6a6a1eef86487
michael-s-teodor <michael.s.teodor@gmail.com>
2019-12-19 13:31:32 +0100

    updated address

--- code/main.py
+++ code/main.py
@@ -5,7 +5,7 @@ from recogniser import Recogniser
 from network import Network
 
 #### Main
-img = Preprocessor("samples/2.jpg")
+img = Preprocessor("../samples/2.jpg")
 recogniser = Recogniser()
 img.process_image()
 img.convert_to_binary()

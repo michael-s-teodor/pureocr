3be9d2a1f07612ca19ddd9b023e17e0c7bbdb36d
michael-s-teodor <michael.s.teodor@gmail.com>
2019-12-26 08:36:32 +0100

    update

--- code/network_new.py
+++ code/network_new.py
@@ -11,33 +11,42 @@ import numpy as np
 #### Class declaration
 class NeuralNetwork:
     def __init__(self, x, y, sizes):
-        self.layer_num = len(sizes)+2
+        self.layer_num = len(sizes)+1
         self.layer = [[]] * self.layer_num
-        self.layer[0] = x # input layer
+        self.input = x
         self.weights = [[]] * self.layer_num
-        # self.weights[1]= np.random.rand(self.layer[0].shape[1], 4) # considering we have 4 nodes in the hidden layer
-        # self.weights[2] = np.random.rand(4, 1)
-        self.weights[1]= np.random.rand(x.shape[1], sizes[0])
-        for i in range(2, self.layer_num-1):
+        self.weights[0]= np.random.rand(self.input.shape[1], sizes[0])
+        for i in range(1, self.layer_num-1):
             self.weights[i] = np.random.rand(sizes[i-1], sizes[i])
-        self.weights[self.layer_num-1] = np.random.rand(sizes[self.layer_num-3], y.shape[1])
+        self.weights[self.layer_num-1] = np.random.rand(sizes[self.layer_num-2], y.shape[1])
         self.y = y
         self.output = np.zeros(y.shape)
 
     def feed_forward(self):
+        self.layer[0] = sigmoid(np.dot(self.input, self.weights[0]))
         for i in range(1, self.layer_num):
             self.layer[i] = sigmoid(np.dot(self.layer[i-1], self.weights[i]))
         return self.layer[self.layer_num-1]
     
+    def rec_back_prop(self, d_weights, index):
+        # d_weights[1] = np.dot(self.layer[0].T, 2*(self.y - self.output)*sigmoid_derivative(self.output))
+        # d_weights[0] = np.dot(self.input.T, np.dot(2*(self.y - self.output)*sigmoid_derivative(self.output), self.weights[1].T)*sigmoid_derivative(self.layer[0]))
+        
+        # base case
+        if (index == 0):
+            return
+
+        # recursive case
+        self.rec_back_prop(d_weights, index-1)
+    
     def back_prop(self):
         d_weights = [[]] * self.layer_num
-
-        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-        d_weights[2] = np.dot(self.layer[1].T, 2*(self.y -self.output)*sigmoid_derivative(self.output))
-        d_weights[1] = np.dot(self.layer[0].T, np.dot(2*(self.y -self.output)*sigmoid_derivative(self.output), self.weights[2].T)*sigmoid_derivative(self.layer[1]))
+        
+        # recursive application of the chain rule to find derivative of the loss function with respect to each weight
+        self.rec_back_prop(d_weights, self.layer_num-1)
 
         # update the weights with the derivative (slope) of the loss function
-        for i in range(1, self.layer_num):
+        for i in range(0, self.layer_num):
             self.weights[i] += d_weights[i]
 
     def train(self, x, y):
@@ -65,11 +74,11 @@ if __name__ == "__main__":
                   [1],
                   [0]), dtype=float)
 
-    # sizes of each hidden layer
+    # size of each hidden layer
     sizes = [4]
 
     NN = NeuralNetwork(x, y, sizes)
-    for i in range(1500): # trains the NN 1,000 times
+    for i in range(1001): # trains the NN 1,000 times
         if i % 100 ==0: 
             print ("for iteration # " + str(i) + "\n")
             print ("Input : \n" + str(x))
